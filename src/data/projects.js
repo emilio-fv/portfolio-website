1873cf6bb136167fe1d7883d770b80c86267b81d
emilio-fv <emiliofv.sd@gmail.com>
2023-05-07 13:04:53 -0700

    update github link

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ const projectData = [
     // Project-S
     createProject("Project-S", "https://github.com/emilio-fv/Project-S", "A MERN-based project management and bug-tracking web application designed for seamless collaboration among software development teams.", ["Mongoose", "MongoDB", "Express", "JavaScript", "React", "Redux", "Material UI"], ProjectSImg, "Project-S project logo"),
     // Vesta
-    createProject("Vesta", "", "A dynamic e-commerce website for a fake designer with an admin interface to update the products in inventory.", ["Express", "JavaScript", "React", "Redux", "Material UI"], VestaImg, "Vesta project logo")
+    createProject("Vesta", "https://github.com/emilio-fv/Vesta", "A dynamic e-commerce website for a fake designer with an admin interface to update the products in inventory.", ["Express", "JavaScript", "React", "Redux", "Material UI"], VestaImg, "Vesta project logo")
 ];
 
 export default projectData;
