adade627f785e2a16fb85f400755721e53cd8d91
emilio-fv <emiliofv.sd@gmail.com>
2023-05-30 11:47:12 -0700

    update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -27,9 +27,30 @@ const Navbar = () => {
                     <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            {/* Menu */}
-            <div 
+            {/* Menu (Mobile) */}
+            <div
                 className={`w-full block flex-grow md:flex md:items-center md:w-auto md:bg-inherit ${menuOpen === true ? 'bg-accent-medium' : 'hidden'}`}>
+                <ul class={`text-sm md:flex-grow md:items-center flex flex-col items-end py-3 gap-1 pr-4 md:flex-row md:mb-6 md:justify-end md:gap-6`}>                    
+                    <li>
+                        <AnchorLink to="/#about" className='hover:underline text-lg'>About</AnchorLink>
+                    </li>
+                    <li>
+                        <AnchorLink to="/#projects" className='hover:underline text-lg'>Projects</AnchorLink>
+                    </li>
+                    <li>
+                        <AnchorLink to="/#experience" className='hover:underline text-lg'>Experience</AnchorLink>
+                    </li>
+                    <li>
+                        <AnchorLink to="/#contact" className='hover:underline text-lg'>Contact</AnchorLink>
+                    </li>
+                    <li>
+                        <button onClick={event => handleResumeClick()} className='px-4 py-1 border-solid border-[.5px] border-white hover:border-slate-400 text-lg'>Resume</button>
+                    </li>
+                </ul>
+            </div>
+            {/* Menu Desktop */}
+            <div
+                className={`w-full block sm:hidden flex-grow md:flex md:items-center md:w-auto md:bg-inherit`}>
                 <ul class={`text-sm md:flex-grow md:items-center flex flex-col items-end py-3 gap-1 pr-4 md:flex-row md:mb-6 md:justify-end md:gap-6`}>                    
                     <li>
                         <AnchorLink to="/#about" className='hover:underline text-lg'>About</AnchorLink>
